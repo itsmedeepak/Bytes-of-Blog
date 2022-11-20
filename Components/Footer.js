9697c3fcd3b99d0688fea7d2b4bc26afa484c717
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2022-11-20 20:57:35 +0530

    Add files via upload

---
+++ Components/Footer.js
@@ -0,0 +1,62 @@
+import { SiTwitter, SiGithub, SiInstagram } from "react-icons/si";
+import { FiLinkedin } from "react-icons/fi";
+
+function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer className="absolute w-full -bottom-0">
+      <div className="bg-indigo-600 dark:bg-indigo-900">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-gray-50 text-sm text-center sm:text-left">
+            © {year} Bytes-0f-Blog —
+            <a
+              href="https://twitter.com/itsmeedepak"
+              rel="noopener noreferrer"
+              className="text-gray-50 ml-1"
+              target="_blank"
+            >
+              @ Deepak
+            </a>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a
+              className="text-gray-50"
+              href="https://twitter.com/itsmeedepak"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <SiTwitter />
+            </a>
+            <a
+              className="ml-4 text-gray-50"
+              href="https://github.com/itsmedeepak"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <SiGithub />
+            </a>
+            <a
+              className="ml-4 text-gray-50"
+              href="https://www.instagram.com/itsmedeepak071/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <SiInstagram />
+            </a>
+            <a
+              className="ml-4 text-gray-50"
+              href="https://www.linkedin.com/in/programdns/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FiLinkedin />
+            </a>
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
